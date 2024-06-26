36d63e4d357aa4d180842f8a9521c903da9708a4
Lucas Eduardo <lesds2001@gmail.com>
2024-06-25 23:38:45 -0300

    Adicionando classes de categorias e produtos, e seus end points

---
+++ src/main/java/com/educandoweb/course/resources/CategoryResource.java
@@ -0,0 +1,37 @@
+package com.educandoweb.course.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.course.entities.Category;
+import com.educandoweb.course.services.CategoryService;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+	@Autowired
+	private CategoryService CategoryService;
+	
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll(){
+		
+		List<Category> listU = CategoryService.findAll();
+				
+		return ResponseEntity.ok().body(listU);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Category> findById(@PathVariable Long id){
+				
+		Category Category = CategoryService.findById(id);
+		return ResponseEntity.ok().body(Category);
+		
+	}
+}
